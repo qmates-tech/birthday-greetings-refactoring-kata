655b6496dcb71cd2d911a1b6997dd166fd809aea
Daniele Megna <daniele.megna@qmates.tech>
2026-05-27 09:02:04 +0200

    Some refactoring in JakartaSmtpMailClient
    improve readability extracting some private methods
    for mail session and mail building operations

--- Java/src/main/java/it/xpug/kata/birthday_greetings/ports/JakartaSmtpMailClient.java
+++ Java/src/main/java/it/xpug/kata/birthday_greetings/ports/JakartaSmtpMailClient.java
@@ -7,6 +7,8 @@ import jakarta.mail.Transport;
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 
+import java.util.Properties;
+
 public class JakartaSmtpMailClient implements MailClient {
 
 	private final String senderAddress;
@@ -20,19 +22,25 @@ public class JakartaSmtpMailClient implements MailClient {
 	}
 
 	public void sendMessage(String subject, String body, String recipient) throws MessagingException {
-		java.util.Properties props = new java.util.Properties();
-		props.put("mail.smtp.host", smtpHost);
-		props.put("mail.smtp.port", "" + smtpPort);
-		Session session = Session.getInstance(props, null);
-
-		// Construct the message
-		Message msg = new MimeMessage(session);
-		msg.setFrom(new InternetAddress(senderAddress));
-		msg.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
-		msg.setSubject(subject);
-		msg.setText(body);
-
-		// Send the message
+		Session session = createMailSession();
+		Message msg = buildMessage(subject, body, recipient, session);
 		Transport.send(msg);
+	}
+
+	private Session createMailSession() {
+		Properties sessionProperties = new Properties() {{
+			put("mail.smtp.host", smtpHost);
+			put("mail.smtp.port", "" + smtpPort);
+		}};
+		return Session.getInstance(sessionProperties, null);
+	}
+
+	private Message buildMessage(String subject, String body, String recipient, Session session) throws MessagingException {
+		return new MimeMessage(session) {{
+			setFrom(new InternetAddress(senderAddress));
+			setRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+			setSubject(subject);
+			setText(body);
+		}};
 	}
 }
